a36c72352232d5b55ebb74de396ec2fe640fdc0b
Architpant <pantarchit11@gmail.com>
2025-11-29 18:45:39 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,27 +1,46 @@
-variable "resource_group_name" {
-  default = "RG-DevOps-ArchitPant"
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+  default     = ""
 }
 
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "RG-DevOps-ArchitPant"
+}
 
 variable "location" {
-  default = "Central India"
+  description = "Azure region for resources"
+  type        = string
+  default     = "Central India"
 }
-
 
 variable "jenkins_vm_name" {
-  default = "jenkins-vm-archit"
+  description = "Name of Jenkins virtual machine"
+  type        = string
+  default     = "jenkins-vm-archit"
 }
 
-variable "app_vm_name" {
-  default = "docker-vm-archit"
+variable "vm_size" {
+  description = "Size of the virtual machine"
+  type        = string
+  default     = "Standard_B2s"
 }
 
+variable "admin_username" {
+  description = "Admin username for VM"
+  type        = string
+  default     = "azureuser"
+}
 
 variable "tags" {
+  description = "Tags for Azure resources"
+  type        = map(string)
   default = {
-    Project = "DevOps-CICD"
-    Owner   = "Archit Pant"
+    Project    = "DevOps-CICD"
+    Owner      = "Archit Pant"
     University = "Bennett"
+    Environment = "Development"
   }
 }
-
