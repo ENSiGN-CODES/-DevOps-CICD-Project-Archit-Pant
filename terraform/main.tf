024ac44912b62a902a088c23f7d59a4f9a9ea2bb
Architpant <pantarchit11@gmail.com>
2025-11-29 18:44:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,89 @@
 provider "azurerm" {
-  features = {}
+  features {}
+  subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "devops_projects" {
-  name     = "Devops-Projects"
-  location = "East US"
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  
+  tags = var.tags
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vm-ubuntu-vnet"
+  name                = "devops-vnet-archit"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.devops_projects.location
-  resource_group_name = azurerm_resource_group.devops_projects.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  
+  tags = var.tags
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "default"
-  resource_group_name  = azurerm_resource_group.devops_projects.name
+  name                 = "default-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "jenkins-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Jenkins"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  
+  tags = var.tags
 }
 
 resource "azurerm_public_ip" "public_ip" {
-  name                = "jenkins-vm-ip"
-  location            = azurerm_resource_group.devops_projects.location
-  resource_group_name = azurerm_resource_group.devops_projects.name
-  allocation_method   = "Dynamic"
+  name                = "jenkins-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  
+  tags = var.tags
 }
 
 resource "azurerm_network_interface" "jenkins_nic" {
-  name                = "jenkins-vm-nic"
-  location            = azurerm_resource_group.devops_projects.location
-  resource_group_name = azurerm_resource_group.devops_projects.name
+  name                = "jenkins-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -39,27 +91,40 @@ resource "azurerm_network_interface" "jenkins_nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
+  
+  tags = var.tags
 }
 
-resource "azurerm_virtual_machine" "jenkins_vm" {
-  name                  = "Jenkins-vm"
-  location              = azurerm_resource_group.devops_projects.location
-  resource_group_name   = azurerm_resource_group.devops_projects.name
-  network_interface_ids = [azurerm_network_interface.jenkins_nic.id]
-  vm_size               = "Standard_B1ms"
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.jenkins_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
-  storage_os_disk {
-    name              = "jenkins-os-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+resource "azurerm_linux_virtual_machine" "jenkins_vm" {
+  name                = var.jenkins_vm_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.jenkins_nic.id]
+  size               = "Standard_B2s"
+
+  admin_username = "azureuser"
+  
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
+  os_disk {
+    name              = "jenkins-os-disk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
     version   = "latest"
   }
-}
+  
+  tags = var.tags
